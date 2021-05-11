3193655c9be65a3ea49bf941ac92975931d3ac48
Collin Winner <cmwinner89@gmail.com>
2021-05-11 12:30:03 -0400

    fix mmigration

--- db/migrate/20210510160311_create_users.rb
+++ db/migrate/20210510160311_create_users.rb
@@ -10,6 +10,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     end
 
     add_index :users, :email, unique: true
-    add_index :users, :session_token, unqiue: true
+    add_index :users, :session_token, unique: true
   end
 end
