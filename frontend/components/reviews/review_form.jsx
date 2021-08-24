159aa3c10661e463d4ab13916db5d3e6cc789170
Collin Winner <cmwinner89@gmail.com>
2021-08-24 10:30:25 -0400

    add rating: idx to update updateSelectedStart

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -9,27 +9,29 @@ class ReviewForm extends React.Component {
             ...this.props.review,
             starValueSelected: 1,
             starValueHovered: -1,
-            toggleReviewContainer: false
+            toggleReviewContainer: false,
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.toggleReviewHandler - this.toggleReviewHandler.bind(this);
+        this.toggleReviewHandler = this.toggleReviewHandler.bind(this);
     }
 
     handleSubmit(e) {
         e.preventDefault();
 
+        this.setState({
+            rating: this.state.starValueSelected
+        });
+
         this.props.action(this.state);
         this.toggleReviewHandler();
-        
+
         this.setState({
-           post_date: '',
-           rating: 1,
-           review: '' 
+            post_date: '',
+            rating: 1,
+            review: ''
         });
     }
-
-    
 
     toggleReviewHandler() {
         this.setState({ ...this.state, toggleReviewContainer: !this.state.toggleReviewContainer })
@@ -38,10 +40,13 @@ class ReviewForm extends React.Component {
     updateStarValueHovered(idx) {
         this.setState({ ...this.state, starValueHovered: idx });
     }
-    
-    
+
     updateSelectedStar(idx) {
-        this.setState({ ...this.state, starValueSelected: idx});
+        this.setState({
+            ...this.state,
+            starValueSelected: idx,
+            rating: idx
+        });
     }
 
     update(field) {
@@ -93,19 +98,19 @@ class ReviewForm extends React.Component {
                             <div className="review-rating-stars">
                                 <div onMouseLeave={() => this.setState({ ...this.state, starValueHovered: -1 })} onMouseEnter={() => this.updateStarValueHovered(1)} onClick={() => this.updateSelectedStar(1)} className={`star ${this.state.starValueHovered !== -1 && 1 <= this.state.starValueHovered ? 'active-star' : ''} ${this.state.starValueHovered === -1 && this.state.starValueSelected >= 1 ? 'active-star' : ''}`}>
                                     ★
-                            </div>
+                                </div>
                                 <div onMouseLeave={() => this.setState({ ...this.state, starValueHovered: -1 })} onMouseEnter={() => this.updateStarValueHovered(2)} onClick={() => this.updateSelectedStar(2)} className={`star ${this.state.starValueHovered !== -1 && 2 <= this.state.starValueHovered ? 'active-star' : ''} ${this.state.starValueHovered === -1 && this.state.starValueSelected >= 2 ? 'active-star' : ''}`}>
                                     ★
-                            </div>
+                                </div>
                                 <div onMouseLeave={() => this.setState({ ...this.state, starValueHovered: -1 })} onMouseEnter={() => this.updateStarValueHovered(3)} onClick={() => this.updateSelectedStar(3)} className={`star ${this.state.starValueHovered !== -1 && 3 <= this.state.starValueHovered ? 'active-star' : ''} ${this.state.starValueHovered === -1 && this.state.starValueSelected >= 3 ? 'active-star' : ''}`}>
                                     ★
-                            </div>
+                                </div>
                                 <div onMouseLeave={() => this.setState({ ...this.state, starValueHovered: -1 })} onMouseEnter={() => this.updateStarValueHovered(4)} onClick={() => this.updateSelectedStar(4)} className={`star ${this.state.starValueHovered !== -1 && 4 <= this.state.starValueHovered ? 'active-star' : ''} ${this.state.starValueHovered === -1 && this.state.starValueSelected >= 4 ? 'active-star' : ''}`}>
                                     ★
-                            </div>
+                                </div>
                                 <div onMouseLeave={() => this.setState({ ...this.state, starValueHovered: -1 })} onMouseEnter={() => this.updateStarValueHovered(5)} onClick={() => this.updateSelectedStar(5)} className={`star ${this.state.starValueHovered !== -1 && 5 <= this.state.starValueHovered ? 'active-star' : ''} ${this.state.starValueHovered === -1 && this.state.starValueSelected >= 5 ? 'active-star' : ''}`}>
                                     ★
-                            </div>
+                                </div>
                             </div>
                         </div>
 
